8df21fa1ed6e607db321864e1f8943fced39a608
TkaczKacper <kacpertk2016@gmail.com>
2024-11-07 12:23:56 +0100

    on swipe accept/reject movie

--- tinder-for-movies/src/components/MovieCard.jsx
+++ tinder-for-movies/src/components/MovieCard.jsx
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { acceptMovie, rejectMovie } from "../redux/actions";
 
 export const MovieCard = ({ movie }) => {
+   const [touchStart, setTouchStart] = useState(null);
+   const [touchEnd, setTouchEnd] = useState(null);
+   const [info, setInfo] = useState(null);
+   const minSwipe = 50;
+   const dispatch = useDispatch();
+
+   const onStart = (pos) => {
+      setTouchEnd(null);
+      setTouchStart(pos);
+   };
+
+   const onMove = (pos) => setTouchEnd(pos);
+   const onEnd = () => {
+      if (!touchStart || !touchEnd) return;
+
+      const distance = touchStart - touchEnd;
+      const isLeftSwipe = distance > minSwipe;
+      const isRightSwpie = distance < -minSwipe;
+
+      if (isLeftSwipe || isRightSwpie) {
+         console.log("swpie", isLeftSwipe ? "left" : "right");
+         setInfo(isLeftSwipe ? "left" : "right");
+         isLeftSwipe
+            ? dispatch(rejectMovie(movie.id))
+            : dispatch(acceptMovie(movie.id));
+      }
+
+      setTouchStart(null);
+      setTouchEnd(null);
+   };
+
+   // desktop version swipe event handlers
+   const onMouseDown = (e) => onStart(e.clientX);
+   const onMouseMove = (e) => onMove(e.clientX);
+   const onMouseUp = () => onEnd();
+
+   // mobile version swipe event handlers
+   const onTouchStart = (e) => onStart(e.targetTouches[0].clientX);
+   const onTouchMove = (e) => onMove(e.targetTouches[0].clientX);
+   const onTouchEnd = () => onEnd();
+
    return (
-      <div className="movie-card">
+      <div
+         className="movie-card"
+         onTouchStart={onTouchStart}
+         onTouchMove={onTouchMove}
+         onTouchEnd={onTouchEnd}
+         onMouseDown={onMouseDown}
+         onMouseMove={(e) => touchStart && onMouseMove(e)}
+         onMouseUp={onMouseUp}
+      >
+         <h3>{info}</h3>
          <h1 className="movie-title">{movie.title}</h1>
          <img
             className="movie-image"
